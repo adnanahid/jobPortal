7702af3a02aaf73e4b0b7365ccc6391558ca2156
adnanahid <sikuahamed@gmail.com>
2024-12-13 11:08:03 +0600

    loading spiner is added and eddited

--- src/Router/PrivateRoute.jsx
+++ src/Router/PrivateRoute.jsx
@@ -1,13 +1,17 @@
 import React, { useContext } from "react";
 import AuthContext from "../Context/AuthContext";
 import { Navigate, useLocation } from "react-router-dom";
+import { div } from "motion/react-client";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
-  const location = useLocation()
-  console.log(location);
+  const location = useLocation();
   if (loading) {
-    return <span className="loading loading-spinner loading-lg"></span>;
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
   }
   if (user) {
     return children;
